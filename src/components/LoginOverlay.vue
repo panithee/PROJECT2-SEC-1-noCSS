b6cd8b25865d226ae9bbfe66ba007cdfdfb58b57
cn-niyom <chalunthorn.niyom@gmail.com>
2023-03-17 11:29:58 +0700

    ....

--- src/components/LoginOverlay.vue
+++ src/components/LoginOverlay.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { eventBus } from '../eventbus.vue'
 const closeLoginOverlay = () => {
     eventBus.$emit('closeLoginOverlay')
 }
@@ -7,13 +6,13 @@ const closeLoginOverlay = () => {
 <template>
     <div class="fixed inset-0 z-50 flex items-center justify-center">
         <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
-        <div class="absolute bg-white container h-full px-6 py-24">
-            <div class="flex h-full flex-wrap items-center justify-center lg:justify-between">
+        <div class="container absolute h-full px-6 py-24 bg-white">
+            <div class="flex flex-wrap items-center justify-center h-full lg:justify-between">
                 <!-- close button -->
                 <div class="absolute top-0 right-0 mt-4 mr-4">
                     <button @click="$emit('close')"
                         class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600">
-                        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                        <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
                             <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" />
                         </svg>
@@ -26,33 +25,33 @@ const closeLoginOverlay = () => {
                 <div class="md:w-8/12 lg:ml-6 lg:w-5/12">
                     <form>
                         <!-- Email input -->
-                        <div class="form-control  basis-full max-w-md">
+                        <div class="max-w-md form-control basis-full">
                             <label class="label">
                                 <span class="label-text">Username</span>
                             </label>
-                            <input type="text" placeholder="Type here" class="input input-bordered w-full " />
-                            <label class="label invisible ">
+                            <input type="text" placeholder="Type here" class="w-full input input-bordered " />
+                            <label class="invisible label ">
                                 <span class="label-text-alt">Error</span>
                             </label>
                         </div>
                         <!-- Password input -->
-                        <div class="form-control basis-full max-w-md">
+                        <div class="max-w-md form-control basis-full">
                             <label class="label ">
                                 <span class="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="Type here" class="input input-bordered w-full" />
-                            <label class="label invisible ">
+                            <input type="text" placeholder="Type here" class="w-full input input-bordered" />
+                            <label class="invisible label ">
                                 <span class="label-text-alt">Error</span>
                             </label>
                         </div>
 
 
-                        <div class="mb-6  items-center justify-between max-w-md flex basis-full">
+                        <div class="flex items-center justify-between max-w-md mb-6 basis-full">
 
                         </div>
 
                         <!-- Submit button -->
-                        <button type="submit" class="btn w-full max-w-md"> Sign in</button>
+                        <button type="submit" class="w-full max-w-md btn"> Sign in</button>
 
                     </form>
                 </div>
