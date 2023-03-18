ef8b09c068e6c38081aa545d6383b26ebd429f12
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-03-18 18:01:24 +0700

    percent-page

--- src/components/DivideCost.vue
+++ src/components/DivideCost.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, computed } from "vue"
 
-
 const page = ref(true)
 
 const switchMenu = (type) => {
@@ -12,25 +11,30 @@ const switchMenu = (type) => {
   }
 }
 
-const foodList = ref([{foodname: "น้ำตก", price: 293}])
+const foodList = ref([{ foodname: "น้ำตก", price: 293 }])
 
 
-const personList = ref([{name:"John", status:false, price:0}, {name:"Three", status:false, price:0}, 
-{name:"Eve", status:false, price:0}, {name:"Mo", status:false, price:0}, {name:"A", status:false, price:0}, {name:"B", status:false, price:0}, {name:"C", status:false, price:0}])
+const personList = ref([{ name: "John", status: false, price: 0, percent: 0 }, { name: "Three", status: false, price: 0, percent: 0 },
+{ name: "Eve", status: false, price: 0, percent: 0 }, { name: "Mo", status: false, price: 0, percent: 0 }, { name: "A", status: false, price: 0, percent: 0 },
+{ name: "B", status: false, price: 0, percent: 0 }, { name: "C", status: false, price: 0, percent: 0 },
+{ name: "e", status: false, price: 0, percent: 0 }, { name: "d", status: false, price: 0, percent: 0 },
+{ name: "F", status: false, price: 0, percent: 0 }, { name: "G", status: false, price: 0, percent: 0 },
+{ name: "H", status: false, price: 0, percent: 0 }, { name: "I", status: false, price: 0, percent: 0 }])
 
 const personsWhoEat = ref([])
+
 
 
 const togglePersonWhoEat = (event) => {
   const index = event.target.id;
   personList.value[index].status = !personList.value[index].status
-  
-  if(personList.value[index].status===true){
-    if(!personsWhoEat.value.includes(personList.value[index])){
+
+  if (personList.value[index].status === true) {
+    if (!personsWhoEat.value.includes(personList.value[index])) {
       personsWhoEat.value.push(personList.value[index])
       console.log(personsWhoEat.value)
     }
-  }else if(personList.value[index].status===false){
+  } else if (personList.value[index].status === false) {
     personsWhoEat.value = personsWhoEat.value.filter((person) => person !== personList.value[index])
     console.log(personsWhoEat.value)
   }
@@ -39,78 +43,107 @@ const togglePersonWhoEat = (event) => {
 const priceEachPerson = ref(0)
 
 const avgPricePerPerson = computed(() => {
-  if(personsWhoEat.value.length <= 0){
+  if (personsWhoEat.value.length <= 0) {
     return 0
   }
   const price = foodList.value[0].price
   const numPersons = personsWhoEat.value.length
   const result = price / numPersons
-  return Math.ceil(result*100) / 100
+  return Math.ceil(result * 100) / 100
 });
 
 
 const totalPrice = computed(() => foodList.value[0].price)
 
+const props = defineProps({
+  percent: Number,
+  default: 0
+})
+
+
+const setPercentOfPerson = () => {
+
+}
+
+const totalPercent = () => {
+  const result = personsWhoEat.value.reduce((total, person) => {
+    total + person.percent
+  }, 0)
+}
+
+
+
 </script>
 
 <template>
-    <div class="w-full h-full">
-      <p class="flex text-3xl" @click="">Back</p>
-      <p class="flex justify-center text-4xl pt-5">น้ำตก</p>
-      <p class="flex justify-center text-lg mt-5 text-gray-600">Average by</p>
-      <div class="flex justify-center">
-        <img
-          src="../assets/AkarIconsEqual.svg"
-          class="mr-2 cursor-pointer"
-          @click="switchMenu('equal')"
-          :class="page === true ? 'opacity-100' : 'opacity-10'"
-        />
-        <img
-          src="../assets/MdiPercentCircleOutline.svg"
-          class="cursor-pointer"
-          @click="switchMenu('percent')"
-          :class="page === false ? 'opacity-100' : 'opacity-10'"
-        />
-      </div>
-
-      <p class="flex justify-center text-lg text-gray-600 mt-5">Member</p>
-      <div class="w-1/2 flex bg-white justify-center mx-auto">
-        <button class="border border-black rounded-lg px-2 mx-2" v-for="(person,index) in personList" :key="index" :id="index"
-        @click="togglePersonWhoEat($event)" :class="(person.status===true)?'bg-white hover:bg-gray-200  border-black text-black' : 'bg-white hover:bg-gray-200 hover:text-black border-gray-200 text-gray-300'">
-          {{ person.name }}
-        </button>
-      </div>
-
-      <table class="w-3/5 mx-auto mt-3" v-if="page">
-        <thead>
-          <tr>
-            <th class="text-left">คนจ่าย</th>
-            <th class="text-end">ราคา</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="border-b border-gray-300 " v-for="(person,index) in personsWhoEat" :key="index">
-            <td class="text-left text-xl" >{{ person.name }}</td>
-            <td class="text-end text-xl">{{ avgPricePerPerson }} </td>
-          </tr>
-        </tbody>
-      </table>
-
-      <div v-if="!page">
-        <p>คนจ่าย</p>
-        <p>ราคา</p>
-      </div>
+  <div class="w-full h-full">
+    <p class="flex text-3xl" @click="">Back</p>
+    <p class="flex justify-center text-4xl pt-5">น้ำตก</p>
+    <p class="flex justify-center text-lg mt-5 text-gray-600">Average by</p>
+    <div class="flex justify-center">
+      <img src="../assets/AkarIconsEqual.svg" class="mr-2 cursor-pointer" @click="switchMenu('equal')"
+        :class="page === true ? 'opacity-100' : 'opacity-10'" />
+      <img src="../assets/MdiPercentCircleOutline.svg" class="cursor-pointer" @click="switchMenu('percent')"
+        :class="page === false ? 'opacity-100' : 'opacity-10'" />
     </div>
 
-      <div>
-        <button class="flex justify-center mt-48 text-xl w-fit mx-auto">Done</button>
-      <button class="flex justify-center mt-1 text-xl text-gray-400 underline w-fit mx-auto">Delete</button>
-      <p class="flex underline justify-center ml-96 w-fit text-2xl">Total : {{ totalPrice }}</p>
+    <p class="flex justify-center text-lg text-gray-600 mt-5">Member</p>
+    <div class="w-1/2 flex bg-white justify-center mx-auto">
+      <button class="border border-black rounded-lg px-2 mx-2" v-for="(person, index) in personList" :key="index"
+        :id="index" @click="togglePersonWhoEat($event)"
+        :class="(person.status === true) ? 'bg-white hover:bg-gray-200  border-black text-black' : 'bg-white hover:bg-gray-200 hover:text-black border-gray-200 text-gray-300'">
+        {{ person.name }}
+      </button>
+    </div>
 
-      </div>
-     
-      
 
+    <table class="w-3/5 mx-auto mt-4" v-if="page">
+      <thead>
+        <tr>
+          <th class="text-left">คนจ่าย</th>
+          <th class="text-end">ราคา</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        <tr class="border-b border-gray-300 " v-for="(person, index) in personsWhoEat" :key="index">
+          <td class="text-left text-xl">{{ person.name }}</td>
+          <td class="text-end text-xl">{{ avgPricePerPerson }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+
+
+
+    <table class="w-3/5 mx-auto mt-4" v-if="!page">
+      <thead>
+        <tr>
+          <th class="text-left">คนจ่าย</th>
+          <img src="../assets/MaterialSymbolsPercentSharp.svg" class="mx-auto" />
+          <th class="text-end">ราคา</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="border-b border-gray-300 " v-for="(person, index) in personsWhoEat" :key="index">
+          <td class="text-left text-xl">{{ person.name }}</td>
+          <td class="text-center">
+            <input type="number" placeholder="0-100" min="0" max="100" class="bg-gray-200 w-20 text-center" />
+          </td>
+          <td class="text-end text-xl">{{ avgPricePerPerson }} </td>
+        </tr>
+      </tbody>
+    </table>
+
+
+    <button class="flex justify-center mt-10 text-xl w-fit mx-auto">Done</button>
+    <button class="flex justify-center mt-1 text-xl text-gray-400 underline w-fit mx-auto">Delete</button>
+    <p class="flex underline justify-center ml-96 w-fit text-2xl">Total : {{ totalPrice }}</p>
+
+
+
+
+  </div>
 </template>
 
 <style scoped></style>
