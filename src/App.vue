2e3a5b76860fa0ac45b6a4ee67f2ca4ce752a997
cn-niyom <chalunthorn.niyom@gmail.com>
2023-03-26 22:10:46 +0700

    moniyom

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,67 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 import Navbar from './components/Navbar.vue'
-import { onBeforeMount, ref } from 'vue'
-import { findKey, getUserGroups, updateGroups } from './composable/FetchFunctions.js';
+import { findKey, getUserGroups, updateGroups } from './composable/FetchFunctions.js'
 
-const username = ref('');
-const userData = ref([]);
-const loginAlready = ref(true);
-
-const setUsername = (name) => {
-  username.value = name;
-  loginAlready.value = false;
-  fetchUserData();
-};
+const username = ref('')
+const userData = ref([])
+const loginAlready = ref(true)
 
 const fetchUserData = async () => {
-  userData.value = await getUserGroups(username.value);
-  console.log('finishBefore', userData.value);
-};
+  userData.value = await getUserGroups(username.value)
+}
 
-const clearUserData = () => {
-  loginAlready.value = true;
-  userData.value = [];
-  username.value = '';
-  sessionStorage.clear();
-};
-
-onBeforeMount(async () => {
-  console.log('before mount');
-  const storedUsername = sessionStorage.getItem('username');
+const checkLogin = async () => {
+  const storedUsername = sessionStorage.getItem('username')
   if (!storedUsername) {
-    userData.value = [];
-    loginAlready.value = true;
-    return;
+    userData.value = []
+    loginAlready.value = true
+    return
   }
   try {
-    const userKey = await findKey(storedUsername);
-    const key = sessionStorage.getItem('key');
+    const userKey = await findKey(storedUsername)
+    const key = sessionStorage.getItem('key')
     if (userKey !== key) {
-      sessionStorage.clear();
-      username.value = '';
-      userData.value = [];
+      sessionStorage.clear()
+      username.value = ''
+      userData.value = []
     } else {
-      loginAlready.value = false;
-      username.value = storedUsername;
-      await fetchUserData();
+      loginAlready.value = false
+      username.value = storedUsername
+      await fetchUserData()
     }
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-});
+}
+
+const setUsername = async (name) => {
+  username.value = name
+  loginAlready.value = false
+  await fetchUserData()
+}
+
+const clearUserData = () => {
+  loginAlready.value = true
+  userData.value = []
+  username.value = ''
+  sessionStorage.clear()
+}
 
 const updated = (data) => {
-  updateGroups(username.value, data);
-};
+  updateGroups(username.value, data)
+}
+onMounted(() => {
+  console.log('mounted', username.value)
+})
+
+checkLogin()
 </script>
+
 
 <template>
   <Navbar @clearData=clearUserData @setUsername=setUsername></Navbar>
-  <router-view v-if="username !== ''" :userData="userData" :updated="updated"></router-view>
+  <router-view v-if="username !== ''" :userData="userData"></router-view>
   <div v-show="loginAlready"> ช่วย Login pls</div>
 </template>
 
--- src/views/oatView.vue
+++ src/views/oatView.vue
@@ -3,18 +3,23 @@
     <h1>oatView</h1>
     {{ userData }}
   </div>
+  <br>
+  <br>
+  HI
+  {{ test }}
 </template>
 
 <script setup>
-import { defineProps, onMounted } from 'vue'
+import { defineProps, ref, watch } from 'vue'
 const props = defineProps({
   userData: {
     type: Array,
     default: []
   }
 })
-onMounted(() => {
-  console.log(props.userData.value)
+const test = ref(props.userData.value)
+watch(() => props.userData, (newVal) => {
+  test.value = newVal
 })
 console.log(props.userData.value)
 </script>
