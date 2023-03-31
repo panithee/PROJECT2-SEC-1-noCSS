bb0f597c151d09dee56a7e50ab633903e15b240d
cn-niyom <chalunthorn.niyom@gmail.com>
2023-03-31 23:05:04 +0700

    fix#1

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -1,41 +1,42 @@
 <template>
   <div class="mainView">
     <div class=" h-screen m-[50px]">
-      
-    <span class=" text-[25px]">
+
+      <span class=" text-[25px]">
         รายการมื้ออาหาร
-     </span>
-     <h1>data</h1>
-{{ props.userData }}
-<hr>
- list 
+      </span>
+      <h1>data</h1>
+      {{ props.userData }}
+      <hr>
+      list
 
- {{ g_list }}
-<!--      
+      {{ g_list }}
+      <!--      
      {{ g_list }} -->
-    <!-- {{ userData.filter( e => e.name = "a") }} -->
-     <!-- <div>{{ userData }}</div> -->
-<!-- 
+      <!-- {{ userData.filter( e => e.name = "a") }} -->
+      <!-- <div>{{ userData }}</div> -->
+      <!-- 
      {{ g_list }} -->
-     <label for="underline_select" class="sr-only">Underline select</label>
-<select v-model="g_name" @click="show"  id="underline_select" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-  
-    <option  v-for="item2 in props.userData "  >{{ item2.name }}</option>
-    
-</select>
+      <label for="underline_select" class="sr-only">Underline select</label>
+      <select v-model="g_name" @click="show" id="underline_select"
+        class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
 
-<div>
-  <span> Member list</span>
-  <ul>
+        <option v-for="item2 in props.userData ">{{ item2.name }}</option>
+
+      </select>
+
+      <div>
+        <span> Member list</span>
+        <ul>
           <li v-for="item in g_list.members">
-           {{ item }}
-         
-        </li>
+            {{ item }}
+
+          </li>
         </ul>
-       
-  
-</div>
-   
+
+
+      </div>
+
     </div>
   </div>
 </template>
@@ -50,19 +51,17 @@ const props = defineProps({
     type: Array, default: []
   }
 })
-const show =()=>{
+const show = () => {
   if (g_name.value == "") {
     g_list.value = {}
-  }else{
-    g_list.value = props.userData.find( e => e.name = g_name)
+  } else {
+    g_list.value = JSON.parse(JSON.stringify(props.userData.find(e => e.name == g_name.value)))
   }
- 
+
 }
 
 
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
