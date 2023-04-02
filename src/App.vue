63210f8d2bfe028f2cb6375aa16274ddc7814af1
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-04-02 10:12:30 +0700

    Revert "save"
    This reverts commit 2b94e038d1e8642485987774ba399f41f8c696fa.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,65 @@
 <script setup>
+import { onBeforeMount, ref } from 'vue'
+import Navbar from './components/Navbar.vue'
+import { findKey, getUserGroups, updateGroups } from './composable/FetchFunctions.js'
+import Loading from './components/icons/loading.vue'
+
+const username = ref('')
+const userData = ref([])
+const loginAlready = ref(false)
+const loading = ref(true);
+const fetchUserData = async () => {
+  userData.value = await getUserGroups(username.value)
+}
+
+const checkLogin = async () => {
+  const storedUsername = sessionStorage.getItem('username')
+  if (!storedUsername) {
+    userData.value = []
+    loginAlready.value = false
+    sessionStorage.setItem("isLoggedIn", "false")
+  }
+  try {
+    const userKey = await findKey(storedUsername)
+    const key = sessionStorage.getItem('key')
+    if (userKey !== key) {
+      sessionStorage.clear()
+      username.value = ''
+      userData.value = []
+      sessionStorage.setItem("isLoggedIn", "false")
+      console.log('key not match')
+      loginAlready.value = false
+    } else {
+      loginAlready.value = true
+      username.value = storedUsername
+      await fetchUserData()
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const setUsername = async (name) => {
+  username.value = name
+  loginAlready.value = true
+  await fetchUserData()
+}
+
+const clearUserData = () => {
+  loginAlready.value = false
+  userData.value = []
+  username.value = ''
+  sessionStorage.clear()
+}
+
+const updated = (data) => {
+  updateGroups(username.value, data)
+}
+onBeforeMount(async () => {
+  loginAlready.value = sessionStorage.getItem("isLoggedIn") === "true";
+  await checkLogin();
+  loading.value = false;
+})
 
 </script>
  
